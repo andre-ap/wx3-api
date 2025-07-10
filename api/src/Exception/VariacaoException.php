fab17c731bd89f7fea6ffd0a7e7a6e6d4ec355fa
Andre Pinto <andrepinto.cg@gmail.com>
2025-07-10 16:59:11 -0300

    test: adiciona testes para variação

--- api/src/Exception/VariacaoException.php
+++ api/src/Exception/VariacaoException.php
@@ -8,32 +8,32 @@ class VariacaoException extends Exception
 {
     public static function idInvalido(): self
     {
-        return new self("ID da variação inválido.", 400);
+        return new self("ID da variação inválido", 422);
     }
 
     public static function variacaoNaoEncontrada(): self
     {
-        return new self("Variação não encontrada.", 404);
+        return new self("Variação não encontrada", 404);
     }
 
     public static function produtoIdInvalido(): self
     {
-        return new self("ID do produto inválido.", 400);
+        return new self("ID do produto inválido", 422);
     }
 
     public static function tamanhoInvalido(): self
     {
-        return new self("Tamanho da variação inválido ou muito curto.", 400);
+        return new self("Tamanho da variação inválido ou muito curto.", 422);
     }
 
     public static function estoqueInvalido(): self
     {
-        return new self("Estoque inválido. Deve ser um número inteiro não negativo.", 400);
+        return new self("Estoque inválido. Deve ser um número inteiro não negativo.", 422);
     }
 
     public static function precoInvalido(): self
     {
-        return new self("Preço inválido. Deve ser um número positivo.", 400);
+        return new self("Preço inválido. Deve ser um número positivo.", 422);
     }
 
     public static function variacaoExistente(): self
